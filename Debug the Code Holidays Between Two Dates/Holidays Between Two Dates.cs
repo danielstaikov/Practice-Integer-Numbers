b839cb365278784a20ac04934b30a5cc903ffa1d
Dani0000 <daniel.staikov@gmail.com>
2017-06-15 14:34:08 +0300

    The exersices Multiply Evens by Odds, Debug the Code, olidays Between Two Dates, Price Change Alert added.

---
+++ Debug the Code Holidays Between Two Dates/Holidays Between Two Dates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debug_the_Code_Holidays_Between_Two_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            int holidaysCount = 0;
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                }
+
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
--- Multiply Evens by Odds/Multiply Evens by Odds.cs
+++ Multiply Evens by Odds/Multiply Evens by Odds.cs
@@ -10,7 +10,43 @@ namespace Multiply_Evens_by_Odds
     {
         static void Main(string[] args)
         {
+            int number = Math.Abs( int.Parse(Console.ReadLine()));
 
+            Console.WriteLine(GetMultipleOfEvensAndOdds(number));
+        }
+        static int GetMultipleOfEvensAndOdds(int n)
+        {
+            return GetSumOfEvenDigits(n)* GetSumOfOddDigits(n);
+        }
+        static int GetSumOfEvenDigits(int n)//четен
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum = sum + lastDigit;
+
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int n)//нечетен
+        {
+            int sum = 0;
+            while (n>0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit%2!=0)
+                {
+                    sum =sum +lastDigit ;
+
+                }
+                n /= 10;
+            }
+            return sum;
         }
     }
 }
